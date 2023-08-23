770e7b82ce9c787f423928328b5945e2e2449ac0
Edyn Manning <gardenofedyn910@yahoo.com>
2023-08-22 20:54:36 -0500

    still trying to get page to display. took all of these out of pages file.

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const images = document.querySelectorAll('.image img');
+    const enlargedContainer = document.querySelector('.enlarged-img');
+    const enlargedImage = enlargedContainer.querySelector('img');
+    const closeBtn = document.querySelector('.close-btn');
+
+    images.forEach(image => {
+        image.addEventListener('click', () => {
+            console.log("Image clicked!");
+            console.log("Image source:", image.src);
+            enlargedImage.src = image.src;
+            console.log("Enlarged image source:", enlargedImage.src);
+            enlargedContainer.style.display = 'flex';
+        });
+    });
+
+    closeBtn.addEventListener('click', () => {
+        enlargedContainer.style.display = 'none';
+    });
+});
