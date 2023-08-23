1f6bc2821f49b456371b939ddb57e829ea187ac3
Edyn Manning <gardenofedyn910@yahoo.com>
2023-08-23 12:22:56 -0500

    fixed column layout on mobile

--- script.js
+++ script.js
@@ -18,3 +18,24 @@ document.addEventListener('DOMContentLoaded', function() {
         enlargedContainer.style.display = 'none';
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const images = document.querySelectorAll('.image img');
+    const enlargedContainer = document.querySelector('.enlarged-img1');
+    const enlargedImage = enlargedContainer.querySelector('img');
+    const closeBtn = document.querySelector('.close-btn1');
+
+    images.forEach(image => {
+        image.addEventListener('click', () => {
+            console.log("Image clicked!");
+            console.log("Image source:", image.src);
+            enlargedImage.src = image.src;
+            console.log("Enlarged image source:", enlargedImage.src);
+            enlargedContainer.style.display = 'flex';
+        });
+    });
+
+    closeBtn.addEventListener('click', () => {
+        enlargedContainer.style.display = 'none';
+    });
+});
